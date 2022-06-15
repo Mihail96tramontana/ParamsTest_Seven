7c6b805b189e1fc38d68440f2147835409ea3401
Mikhail Lyubeznov <xelysqgbhju@gmail.com>
2022-06-15 11:11:29 +0300

    test

---
+++ src/test/java/guru/qa/Utvar.java
@@ -0,0 +1,11 @@
+package guru.qa;
+
+public class Utvar {
+TYPEONE("Электроника"),TYPETWO("Неэлектроника");
+
+    public final String desc;
+
+    Utvar(String desc) {
+        this.desc = desc;
+    }
+}
